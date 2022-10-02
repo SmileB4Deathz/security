b00b041b3e2ca9f79f32683677d8be08aa556e9b
SmileB4Deathz <80955493+SmileB4Deathz@users.noreply.github.com>
2022-10-02 22:44:09 +0200

    Create tampertest.js

---
+++ tampertest.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @run-at       document-end
+// @description  test
+// @author       Steffsot
+// @match        https://youchess.ru:3000/*
+// @icon         https://alexsave.com:3000/assets/men.png
+// ==/UserScript==
+
+console.log('bruh');
