640bcf10ae369222f1af717ed9f57a19e07adcb8
SmileB4Deathz <80955493+SmileB4Deathz@users.noreply.github.com>
2024-09-18 23:33:23 +0200

    Create catchRubashka.user.js

---
+++ catchRubashka.user.js
@@ -0,0 +1,160 @@
+// ==UserScript==
+// @name         catchRubashka
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  gotta catch em all!
+// @author       Steffsot
+// @match        https://durakonline.ru/game/*
+// @grant        none
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=durakonline.ru
+// @run-at document-start
+// ==/UserScript==
+
+(function () {
+    const wsHook = {};
+    hookWebSocket();
+    let caught = false;
+    let id = 0;
+    Object.defineProperty(window.WebSocket, 'OPEN', { value: 1 });
+    const notification = new Audio('https://www.mobilesringtones.com/static/p/ringtones/2018/02/01/16451/16451.mp3?title=16451_download_note_iphone_notification_ringtone_apple_sms_ringtones.mp3');
+    window.addEventListener("load", () => {
+        window.onerror = null;
+    });
+
+
+
+    wsHook.after = function (messageEvent, url, wsObject) {
+        const ws = wsObject;
+        let packets = messageEvent.data.split("\x1E");
+        packets.pop();
+        for (const packet of packets) {
+            let data = JSON.parse(packet);
+            switch (data.target) {
+                case "updateUserData": {
+                    id = data.arguments[0].Id;
+                }
+                case "FillShopTab": {
+                    const shopItems = data.arguments[0];
+                    for (const shopItem of shopItems) {
+                        if (shopItem.Type == 2 && shopItem.SubType == 4 && shopItem.price_joker == 800 && id == 5000000000096247) {
+                            const packetToSend = {
+                                arguments: [shopItem.Id],
+                                invocationId: "10",
+                                target: "buyTovar",
+                                type: 1
+                            }
+                            ws.send(JSON.stringify(packetToSend) + "");
+                            console.log("Caught rubashka");
+                            caught = true;
+                            break;
+                        }
+                    }
+                    if (!caught) {
+                        setTimeout(() => {
+                            const packetToSend = {
+                                "arguments": [
+                                    "ShopGenericTab"
+                                ],
+                                "invocationId": "10",
+                                "target": "loadShop",
+                                "type": 1
+                            }
+                            ws.send(JSON.stringify(packetToSend) + "");
+                        }, 1e3);
+                        console.log("No rubashka");
+                        notification.play();
+                    }
+                }
+            }
+        }
+        return messageEvent;
+    }
+
+
+    function hookWebSocket() {
+        // Mutable MessageEvent.
+        // Subclasses MessageEvent and makes data, origin and other MessageEvent properites mutatble.
+        function MutableMessageEvent(o) {
+            this.bubbles = o.bubbles || false
+            this.cancelBubble = o.cancelBubble || false
+            this.cancelable = o.cancelable || false
+            this.currentTarget = o.currentTarget || null
+            this.data = o.data || null
+            this.defaultPrevented = o.defaultPrevented || false
+            this.eventPhase = o.eventPhase || 0
+            this.lastEventId = o.lastEventId || ''
+            this.origin = o.origin || ''
+            this.path = o.path || new Array(0)
+            this.ports = o.parts || new Array(0)
+            this.returnValue = o.returnValue || true
+            this.source = o.source || null
+            this.srcElement = o.srcElement || null
+            this.target = o.target || null
+            this.timeStamp = o.timeStamp || null
+            this.type = o.type || 'message'
+            this.__proto__ = o.__proto__ || MessageEvent.__proto__
+        }
+
+        var before = wsHook.before = function (data, url, wsObject) {
+            return data
+        }
+        var after = wsHook.after = function (e, url, wsObject) {
+            return e
+        }
+        var modifyUrl = wsHook.modifyUrl = function (url) {
+            return url
+        }
+        wsHook.resetHooks = function () {
+            wsHook.before = before
+            wsHook.after = after
+            wsHook.modifyUrl = modifyUrl
+        }
+
+        var _WS = window.WebSocket
+        window.WebSocket = function (url, protocols) {
+            var WSObject
+            url = wsHook.modifyUrl(url) || url
+            this.url = url
+            this.protocols = protocols
+            if (!this.protocols) { WSObject = new _WS(url) } else { WSObject = new _WS(url, protocols) }
+
+            var _send = WSObject.send
+            WSObject.send = function (data) {
+                arguments[0] = wsHook.before(data, WSObject.url, WSObject) || data
+                _send.apply(this, arguments)
+            }
+
+            // Events needs to be proxied and bubbled down.
+            WSObject._addEventListener = WSObject.addEventListener
+            WSObject.addEventListener = function () {
+                var eventThis = this
+                // if eventName is 'message'
+                if (arguments[0] === 'message') {
+                    arguments[1] = (function (userFunc) {
+                        return function instrumentAddEventListener() {
+                            arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url, WSObject)
+                            if (arguments[0] === null) return
+                            userFunc.apply(eventThis, arguments)
+                        }
+                    })(arguments[1])
+                }
+                return WSObject._addEventListener.apply(this, arguments)
+            }
+
+            Object.defineProperty(WSObject, 'onmessage', {
+                'set': function () {
+                    var eventThis = this
+                    var userFunc = arguments[0]
+                    var onMessageHandler = function () {
+                        arguments[0] = wsHook.after(new MutableMessageEvent(arguments[0]), WSObject.url, WSObject)
+                        if (arguments[0] === null) return
+                        userFunc.apply(eventThis, arguments)
+                    }
+                    WSObject._addEventListener.apply(this, ['message', onMessageHandler, false])
+                }
+            })
+
+            return WSObject
+        }
+    }
+})();
